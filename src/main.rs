5323c80a81d0b9d944697bbe19fb68d375f081c5
shikharbhardwaj <shikharbhardwaj68@gmail.com>
2023-03-19 11:53:37 +0000

    Send responses immediately

--- src/main.rs
+++ src/main.rs
@@ -24,25 +24,19 @@ async fn main() {
 
 async fn handle_conn(socket: TcpStream) -> redis_starter_rust::Result<()> {
     let mut conn = Connection::new(socket);
-    let mut frames = vec![];
 
     loop {
         if let Some(frame) = conn.read_frame().await? {
             info!("Got frame: {:?}", frame);
-            frames.push(frame);
+            let cmd = Command::from_frame(frame)?;
+            cmd.apply(& mut conn).await?
         } else {
             return Err(format!("Could not parse frame, buffer contents: {}", conn.get_buf()).into())
         }
 
-        if frames.len() >= PIPELINE_MAX_COMMANDS || !conn.is_read_ready().await {
+        if !conn.is_read_ready().await {
             break
         }
-    }
-
-    for frame in frames {
-        let cmd = Command::from_frame(frame)?;
-
-        cmd.apply(& mut conn).await?
     }
 
     Ok(())
