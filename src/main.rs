a4ac701f5c160e62327d83cab277776559078187
shikharbhardwaj <shikharbhardwaj68@gmail.com>
2024-02-29 19:13:00 +0000

    Add support to run on different ports

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,43 @@
-use std::{sync::{Arc}, collections::HashMap, io::Bytes};
+use std::env;
+use std::{sync::Arc, collections::HashMap};
 
 use redis_starter_rust::{Connection, Command, Frame, SharedDb};
 
 use tokio::net::{TcpListener, TcpStream};
-use tokio::sync::{Mutex};
+use tokio::sync::Mutex;
 
 mod log;
+
+struct RedisArgs {
+    port: String,
+}
+
+impl RedisArgs {
+    pub fn new() -> Self {
+        let args: Vec<String> = env::args().collect();
+        let port_number_idx = args.iter().position(|r| r == "--port").or_else(|| Some(args.len())).unwrap() + 1;
+
+        let port: String = match args.get(port_number_idx.clone()) {
+            Some(port) => port.clone(),
+            None => "6379".to_owned()
+        };
+
+        Self{
+            port,
+        }
+    }
+}
 
 
 #[tokio::main]
 async fn main() {
     info!("Logs from your program will appear here!");
 
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    // Get port number from the command line arguments, with default of 6379.
+    let args = RedisArgs::new();
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", args.port)).await.unwrap();
+
+    info!("Listening on port: {}", args.port);
 
     let shared_db = Arc::new(Mutex::new(HashMap::new()));
 
